82167b0b1070a019b79f6bfc8c206ea98073223a
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-13 17:01:43 +0530

    Update 10a.py

--- 10a.py
+++ 10a.py
@@ -1,3 +1,6 @@
+#Mystring is a user-defined Package. find the files countvowel and palin to understand what the package modules do
+
+################################################################################################################################
 import Mystring
 print(dir())
 word=(raw_input("Enter a string : "))
